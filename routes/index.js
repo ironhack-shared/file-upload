4e32014e071b2eded842a04a556e1137ff4343a3
Victor Rodriguez <victor.irix@gmail.com>
2018-04-19 14:10:43 +0200

    Cloudinary

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const multer = require('multer');
+//const multer = require('multer');
 const router  = express.Router();
-
+const uploadCloud = require("../config/cloudinary.js");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-const uploadHandler = multer({ dest: './public/uploads/' });
-router.post('/upload', uploadHandler.single('photo'), (req, res, next) => {
-  console.log("ASDFASD")
+// const uploadHandler = multer({ dest: './public/uploads/' });
+// router.post('/upload', uploadHandler.single('photo'), (req, res, next) => {
+//   console.log("ASDFASD")
+//   console.log(req.body);
+//   console.log(req.file);
+//   res.redirect('/')
+// });
+
+router.post("/uploadCloud", uploadCloud.single("photo"), (req, res, next) => {
+  console.log("ASDFASD");
   console.log(req.body);
   console.log(req.file);
-  res.redirect('/')
+  res.redirect("/");
 });
 
 module.exports = router;
